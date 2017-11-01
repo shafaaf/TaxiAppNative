3d6f879fb96e3515f339545455295d6632807ad7
Shafaaf Hossain <shafaafhossain@shafaafMacbook.local>
2017-11-01 19:33:24 -0400

    sending post request to api to book route working

--- server/routes/bookings.js
+++ server/routes/bookings.js
@@ -1,25 +1,39 @@
-// var express = require("express");
-// var router = express.Router();
-// var mongojs = require("mongojs");
-
-// var db = mongojs("mongodb://shafaaf0:password@ds241875.mlab.com:41875/taxiappnative", ["bookings"]);
-
-// router.get("/bookings", function(req, res, next){
-// 	db.bookings.find(function(err, bookings){
-// 		if(err){
-// 			res.send(err);
-// 		}
-// 		res.json(bookings);
-// 	})
-// }); 
-
-// module.exports = router;
-
 var express = require("express");
 var router = express.Router();
+var mongojs = require("mongojs");
 
-router.get('/', function(req, res) {
-    res.json({ message: 'test works' });   
+var db = mongojs("mongodb://shafaaf0:password@ds241875.mlab.com:41875/taxiappnative", ["bookings"]);
+
+router.get('/bookings', function(req, res) {
+	console.log("\nGET Request at /api/bookings");
+	db.bookings.find(function(err, bookings){
+		if(err){
+			console.log("err is: ", err);
+			res.send(err);
+		}
+		console.log("bookings is: ", bookings);
+		res.json(bookings);
+	});
+});
+
+router.post('/bookings', function(req, res){
+	console.log("\nPOST Request at /api/bookings");
+	console.log("req.body is: ", req.body);
+	var booking = req.body;
+	if((booking == null) || (booking == "") || (Object.keys(booking).length === 0)){
+		res.status(400);
+		res.json({
+			"error": "Bad data!"
+		});
+	}
+	else{
+		db.bookings.save(booking, function(err, savedBooking){
+			if(err){
+				res.send(err);
+			}
+			res.json(savedBooking);
+		});	
+	}
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express    = require('express');        
 var app        = express();               
 var bodyParser = require('body-parser');
-var test = require("./routes/bookings");
+var bookings = require("./routes/bookings");
 
 // This will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -9,22 +9,10 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();
-
-//Test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
-});
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
-app.use('/api2', test);
+// Routes for API
+app.use('/api', bookings);
 
 // START THE SERVER
 // =============================================================================
 app.listen(port);
 console.log('Magic happens on port ' + port);
-
